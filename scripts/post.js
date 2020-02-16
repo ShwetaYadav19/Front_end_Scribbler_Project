c58350d649d0e980cccf47114caee1e8eed78a26
ShwetaYadav19 <shwetuyadav19@gmail.com>
2020-02-16 10:10:11 +0530

    Adding comment section for post.html

--- scripts/post.js
+++ scripts/post.js
@@ -1,6 +1,15 @@
 
 var queryString = new Array();
 
+function onEdit(){
+
+    document.getElementById('post').focus();
+
+};
+
+
+
+
 window.onload = function () {
     if (queryString.length == 0) {
         if (window.location.search.split('?').length > 1) {
@@ -18,6 +27,38 @@ window.onload = function () {
         document.getElementsByClassName("heading-content")[0].innerHTML = heading;
         document.getElementsByClassName("author-name")[0].innerHTML = author;
     }
-
+    document.getElementById('comments').style.visibility = 'hidden';
 
 };
+
+var cnt=0;
+function countLikes(){
+
+ cnt=parseInt(cnt)+parseInt(1);
+ var divData=document.getElementById("showCount");
+ var likeButton = document.getElementById("likeButton");
+ likeButton.innerHTML = "Liked";
+ if(cnt==1) 
+ divData.innerHTML= cnt +" person likes this !";//this part has been edited
+ else
+ divData.innerHTML= cnt +" people like this !";
+
+}
+
+var comments = [];
+function addComment(comment){
+    document.getElementById('comments').style.visibility = 'visible';
+    var comment = comment.value;
+    comments.unshift(comment);
+    $("#comment").val('');
+    console.log(comments);
+    this.comments.forEach(addingComment);
+}
+
+function addingComment(item, index){
+    var temp = document.getElementById("comments").innerHTML;
+    document.getElementById("comments").innerHTML = '<div class="comment">'+item+'</div>' + '</br>';
+    document.getElementById("comments").innerHTML += temp + '</br>';
+    comments.pop();
+}
+
